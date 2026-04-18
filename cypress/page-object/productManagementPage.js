119764c8fefc210c12bca22b8e7752317c94541c
Ams-04 <amasafe82@gmail.com>
2026-04-18 23:26:05 +0100

    added productManagementPage.js

---
+++ cypress/page-object/productManagementPage.js
@@ -0,0 +1,16 @@
+class ProducetManagementPage{
+    admin(){
+        cy.contains('button', 'Admin').click()
+        cy.contains('button', 'Add Product').click()
+        cy.get('[name="name"]').clear().type('Mary Jane Shoes')
+        cy.get('[name="category"]').clear().type('Kids')
+        cy.get('[name="price"]').clear().type('29.99')
+        cy.get('[name="stock"]').clear().type('12')
+        cy.get('[name="image_url"]').clear().type('Image URL')
+        cy.get('[name="description"]').clear().type('Description')
+        cy.contains('button', 'Save').click()
+
+    }
+}
+
+export default ProducetManagementPage
