268e06d849b0de433c8813dceaf396e7788f3460
EdemZikks <ekdziko@yahoo.com>
2026-04-25 07:20:59 +0100

    Fix broken image of Mary Jane Shoes

--- cypress/page-object/productManagementPage.js
+++ cypress/page-object/productManagementPage.js
@@ -6,7 +6,7 @@ class ProducetManagementPage{
         cy.get('[name="category"]').clear().type('Kids')
         cy.get('[name="price"]').clear().type('29.99')
         cy.get('[name="stock"]').clear().type('12')
-        cy.get('[name="image_url"]').clear().type('Image URL')
+        cy.get('[name="image_url"]').clear().type('https://media2.newlookassets.com/i/newlook/923788001/womens/footwear/shoes/black-patent-t-bar-mary-jane-shoes.jpg?strip=true&qlt=50&w=720')
         cy.get('[name="description"]').clear().type('Description')
         cy.contains('button', 'Save').click()
 
